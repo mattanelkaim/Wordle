c60eb8ed95b7f1622371445656f863fb3676b5ba
Mattan Elkaim <matanelkaim2007@gmail.com>
2024-01-18 14:32:36 +0200

    Functionality to move to game screen

--- app/src/main/java/com/example/wordle/WelcomeScreen.java
+++ app/src/main/java/com/example/wordle/WelcomeScreen.java
@@ -2,13 +2,21 @@ package com.example.wordle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+    }
+
+    public void toGameScreen(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
 }
